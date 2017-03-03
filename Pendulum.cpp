3214b9600eb770e0f82ae80635b63def7a4a8ecc
divyaPrakash <divs4debu@gmail.com>
2017-03-03 19:50:19 +0530

    Added color class

--- Pendulum.cpp
+++ Pendulum.cpp
@@ -2,8 +2,23 @@
 #include <GL/glut.h>
 
 using namespace std;
+class Color{
+public :
+    GLfloat red;
+    GLfloat green;
+    GLfloat blue;
+    Color(float r, float g, float b){
+        red = float(r/255.0000);
+        green = float(g/255.0000);
+        blue = float(b/255.0000);
+    }
+};
 void call_back_display(){
+    Color c(0,121,107);
+    GLfloat color[] = {c.red, c.green, c.blue};
     glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(color[0], color[1], color[2], 1.0f);
+    glutPostRedisplay();
     glEnd();
     glFlush();
 }
