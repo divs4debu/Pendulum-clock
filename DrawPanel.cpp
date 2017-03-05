e0296e3a550e1ad5e8fca23569e3dce253c52002
divyaPrakash <divs4debu@gmail.com>
2017-03-05 18:59:39 +0530

    Fix DrawRect

--- DrawPanel.cpp
+++ DrawPanel.cpp
@@ -6,21 +6,39 @@
 class DrawPanel{
 private :
     int height, width;
+    void (*f) (void);
     
 public:
+    DrawPanel (){
+        height = 300;
+        width = 300;
+    }
     DrawPanel(void (*f) (void)){
         height = 300;
         width = 300;
         createWindow(height, width, f);
     }
     
-    void createWindow(size_t height, size_t width, void f (void)){
+    void display(){
+        createWindow(height, width, f);
+    }
+    void createWindow(size_t height, size_t width, void (*f) (void)){
         
         glutInitWindowSize(width, height);
         glutInitWindowPosition(100, 100);
         glutCreateWindow("Pendulum");
         glutDisplayFunc(f);
+    }
     
+    void drawRect(float x,float y ,float length, float width, float angle){
+        glRotatef(angle, 0.0,0.0,1.0);
+        glRectf(x, y,x+length,y-width);
+        glRotatef(-angle, 0.0,0.0,1.0);
+    }
+    
+    void setCallBack(void (*function)(void)){
+        f = (*function);
+    }
     
 };
 #endif
--- Pendulum.cpp
+++ Pendulum.cpp
@@ -1,41 +1,54 @@
 #include <iostream>
 #include <GL/glut.h>
 #include "DrawPanel.cpp"
-
+#include <tgmath.h>
 using namespace std;
 class Color{
 public :
     GLfloat red;
     GLfloat green;
     GLfloat blue;
+    Color(){}
     Color(float r, float g, float b){
+        tranform(r,g,b);
+    }
+    void tranform(float r, float g, float b){
         red = float(r/255.0000);
         green = float(g/255.0000);
         blue = float(b/255.0000);
+    } 
+    void setColor(float r, float g, float b){
+         tranform(r,g,b);
     }
 };
 
+Color c;
+DrawPanel panel;
 
+void drawBob(double angle){
+    
+}
 void call_back_display(){
-    Color c(0,121,107);
-    GLfloat color[] = {c.red, c.green, c.blue};
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    glClearColor(color[0], color[1], color[2], 1.0f);
-    //glutPostRedisplay();
-    glRectf(-.01f, .75f, .01f, -.75f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    
+    c.setColor(12,0,100);
+    glColor3f(c.red, c.green,c.blue);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    panel.drawRect(0,0, .5,.02, 60);
+    
     glEnd();
     glutSwapBuffers();
 }
 
-    glutInitWindowSize(width, height);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Pendulum Clock");
-    glutDisplayFunc(call_back_display);
-}*/
+
 int main(int argc, char** argv){
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE);
-    DrawPanel panel(call_back_display);
+    //DrawPanel panel(call_back_display);
+    panel.setCallBack(call_back_display);
+    panel.display();
     glutMainLoop();
     return 0;
 }
